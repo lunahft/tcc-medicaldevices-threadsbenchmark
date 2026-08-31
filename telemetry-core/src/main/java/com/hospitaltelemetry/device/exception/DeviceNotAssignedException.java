940922e6ff352c7a7ce0470c849afb2f1c14547f
Watapon ACC2 <karolneet@gmail.com>
2026-09-01 00:36:27 +0900

    feat: add constructor to DeviceNotAssignedException for device ID handling

--- telemetry-core/src/main/java/com/hospitaltelemetry/device/exception/DeviceNotAssignedException.java
+++ telemetry-core/src/main/java/com/hospitaltelemetry/device/exception/DeviceNotAssignedException.java
@@ -4,4 +4,7 @@ public class DeviceNotAssignedException extends RuntimeException{
     public DeviceNotAssignedException(String message){
         super(message);
     }
+    public DeviceNotAssignedException(Long deviceId){
+        super("Device is not assigned with id: " + deviceId);
+    }
 }
